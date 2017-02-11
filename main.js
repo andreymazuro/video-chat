5db38d4323582d1b0575cc7003a02ec43eb5ecc1
Andrey Mazuro <andrey.mazuro@icloud.com>
2017-02-11 18:30:48 +0300

    sdf

--- main.js
+++ main.js
@@ -21,7 +21,26 @@ navigator.getUserMedia({video: true, audio: true}, function(stream){
   var Peer = require('simple-peer')
   var servers = {
 iceServers: [
-     {  url: "stun:stun.l.google.com:19302"}
+  {url:'stun:stun01.sipphone.com'},
+{url:'stun:stun.ekiga.net'},
+{url:'stun:stun.fwdnet.net'},
+{url:'stun:stun.ideasip.com'},
+{url:'stun:stun.iptel.org'},
+{url:'stun:stun2.l.google.com:19302'},
+{url:'stun:stun3.l.google.com:19302'},
+{url:'stun:stun4.l.google.com:19302'},
+{url:'stun:stunserver.org'},
+{url:'stun:stun.softjoys.com'},
+{url:'stun:stun.voiparound.com'},
+{url:'stun:stun.voipbuster.com'},
+{url:'stun:stun.voipstunt.com'},
+{url:'stun:stun.voxgratia.org'},
+{url:'stun:stun.xten.com'},
+{
+ url: 'turn:numb.viagenie.ca',
+ credential: 'muazkh',
+ username: 'webrtc@live.com'
+}
     ]
 };
   var peer = new Peer({
